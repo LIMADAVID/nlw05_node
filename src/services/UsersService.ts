8726e38d13e6c80b238ee50eff2538939357c233
LIMADAVID <davidlima.dev@gmail.com>
2021-04-21 16:00:08 -0300

    NLW 05 - Aula 03

---
+++ src/services/UsersService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class UsersService {
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        const usersRepository = getCustomRepository(UsersRepository);
+    }
+
+
+    async create(email: string) {
+        
+
+        const userExists = await this.usersRepository.findOne({
+            email
+        });
+
+        if (userExists) {
+            return userExists;
+        };
+
+        const user = this.usersRepository.create({
+            email
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+
+    }
+}
+
+export { UsersService };
